7f835a5d38f51510c5a661b185d85073e82bf519
parsa <parsasotoudeh6@gmail.com>
2023-07-18 11:30:49 +0330

    add feature: using context manager for signing up users.

--- core/entry.py
+++ core/entry.py
@@ -1,6 +1,83 @@
 import os
 from getpass import getpass
-from models.command import ENTERY_COMMANDS
+import json
+import re
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ValidationError(ValueError):
+    pass
+
+
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    @property
+    def name(self) -> str:
+        return self.__username
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if not re.match('[a-zA-Z]{3,}', value):
+            raise ValidationError("Invalid username!")
+        self.__username = value
+
+    @property
+    def password(self) -> str:
+        return self.__password
+
+    @password.setter
+    def password(self, value: str) -> None:
+        if not re.match('[a-zA-Z0-9]{8,}', value):
+            raise ValidationError("Invalid password!")
+        self.__password = value
+
+    @property
+    def email(self) -> str:
+        return self.__email
+
+    @email.setter
+    def email(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise TypeError('Email must be a string')
+        if not value.endswith('@gmail.com'):
+            raise ValidationError('Invalid email format')
+        self.__email = value
+
+
+class UserDatabase:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        try:
+            with open(self.filename, 'r') as f:
+                self.users = json.load(f)
+        except FileNotFoundError:
+            self.users = []
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        with open(self.filename, 'w') as f:
+            json.dump(self.users, f)
+
+    def add_user(self, user):
+        self.users.append({
+            'name': user.name,
+            'email': user.email,
+            'password': user.password
+            })
+
+    def get_user(self, email):
+        for user in self.users:
+            if user['email'] == email:
+                return User(user['name'], user['email'], user['password'])
+        return None
 
 
 def clear_screen():
@@ -13,31 +90,27 @@ def clear_screen():
 
 
 def entry():
-    users = {
-        'Parsa': {
-            'fullname': 'parsa',
-            'password': 'Parsa6666',
-            'email': 'parsa.ys10@gmail.com'
-            },
-        'user2': {
-            'fullname': 'pourya',
-            'password': 'user2',
-            'email': 'user2@gmail.com'
-            }
-    }
-
     signed_in = False
     while not signed_in:
         clear_screen()
         print('WELCOME TO OUR ONLINE MOBILE SHOP')
-        username = input('username(note: capital and lower char): ')
-        password = getpass('password(note: capital/lower and numers): ')
-        clear_screen()
-        try:
-            command = input('Press signup, signin: ')
-            if command in ENTERY_COMMANDS:
-                execute_action = ENTERY_COMMANDS[command]
-                signed_in = execute_action(username, password, users)
-        except Exception as msg:
-            print(f'An error accured: {msg}')
-    return username
+        command = input('Press signup, signin: ').lower()
+        if command == 'signup':
+            with UserDatabase('users.json') as user_db:
+                name = input('Name: ')
+                email = input('Email: ')
+                password = input('Password: ')
+                user = User(name, email, password)
+                user_db.add_user(user)
+                getpass('signup successfully')
+        if command == 'signin':
+            with UserDatabase('users.json') as user_db:
+                email = input('Email: ')
+                password = input('Password: ')
+                user = user_db.get_user(email)
+                if user is not None and user.password == password:
+                    getpass('Login successful!')
+                    signed_in = True
+                else:
+                    getpass('Incorrect email or password.')
+    return name
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from core.entry import entry
 from core.jobs import jobs
 from core.payment import payment
+from conf import *
 name = entry()
 basket = jobs(name)
 total = payment(basket)
